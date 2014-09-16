92bb0c2c27b67296ef952c45cf13e81f8cf3e7ad
Telmo Pires <telmo.pires@gmail.com>
2014-09-16 16:09:33 +0000

    new tests added

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,30 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
+    /*describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
 
             });
         });
+    });*/
+    
+    describe("DOM Tests", function () {
+    /*var el = document.createElement("div");
+    el.id = "myDiv";
+    el.innerHTML = "Hi there!";
+    el.style.background = "#ccc";
+    document.body.appendChild(el);*/
+ 
+    var myEl = document.getElementsByClassName('logo-tagline');
+    it("is in the DOM", function () {
+        expect(myEl).to.not.equal(null);
     });
+ 
+    it("has the right text", function () {
+        expect(myEl.innerHTML).to.equal("É TEMPO DE FAMÍLIA");
+    });
+ 
+});
+    
 })();
